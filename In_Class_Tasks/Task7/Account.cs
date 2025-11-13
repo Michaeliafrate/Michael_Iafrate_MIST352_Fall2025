ba59d95c61d5531deeb7362a62cd8c0a221a1137
Michaeliafrate <mli00003@mix.wvu.edu>
2025-11-13 12:45:55 -0500

    Task7

---
+++ In_Class_Tasks/Task7/Account.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Exam2_Review_Codes
+{
+    public class Account
+    {
+        public int id;
+        public string type;
+        public Customer customer;
+        public double balance;
+        public DateTime openDate;
+
+        public Account(int i, string t)
+        {
+            if (i > 0) id = i;
+            else Console.WriteLine("Id must be > 0");
+
+            if (!string.IsNullOrWhiteSpace(t)) type = t;
+            else Console.WriteLine("Type cannot be empty");
+
+            openDate = DateTime.Now;
+        }
+
+        public Account(Customer c)
+        {
+            if (c != null) customer = c;
+            else Console.WriteLine("Customer cannot be null");
+            openDate = DateTime.Now;
+        }
+
+        public void Deposit(double amt)
+        {
+            if (amt > 0) balance += amt;
+            else Console.WriteLine("Deposit must be > 0");
+        }
+
+        public void Withdraw(double amt)
+        {
+            if (amt <= 0) Console.WriteLine("Withdraw must be > 0");
+            else if (amt > balance) Console.WriteLine("Insufficient funds");
+            else balance -= amt;
+        }
+
+        public void Close()
+        {
+            if (balance == 0) Console.WriteLine("Account closed");
+            else Console.WriteLine("Balance must be zero to close");
+        }
+
+        public void AssignCustomer(Customer c)
+        {
+            if (c != null) customer = c;
+            else Console.WriteLine("Customer cannot be null");
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Account Id: " + id + ", Type: " + type + ", Balance: " + balance + ", Opened: " + openDate);
+            if (customer != null) customer.DisplayInfo();
+        }
+    }
+}
+
---
+++ In_Class_Tasks/Task7/Customer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exam2_Review_Codes
+{
+    public class Customer
+    {
+        public int customerId;
+        public string name;
+        public string email;
+        public string phone;
+        public string address;
+
+        public Customer(int id, string n, string e)
+        {
+            if (id > 0) customerId = id;
+            else Console.WriteLine("CustomerId must be > 0");
+
+            if (!string.IsNullOrWhiteSpace(n)) name = n;
+            else Console.WriteLine("Name cannot be empty");
+
+            if (!string.IsNullOrWhiteSpace(e) && e.Contains("@")) email = e;
+            else Console.WriteLine("Invalid email");
+
+            phone = "";
+            address = "";
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine("Customer Id: " + customerId + ", Name: " + name + ", Email: " + email);
+            Console.WriteLine("Phone: " + (string.IsNullOrEmpty(phone) ? "Not provided" : phone));
+            Console.WriteLine("Address: " + (string.IsNullOrEmpty(address) ? "Not provided" : address));
+        }
+    }
+}
+
+
